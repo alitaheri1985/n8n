ad2de27c8579ede4d1595e7ed73c8d30b9e9bff5
MATIN0023 <57063111+MATIN0023@users.noreply.github.com>
2025-05-21 15:52:24 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,65 @@
-terraform {
-  required_providers {
-    oci = {
-      source = "oracle/oci"
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "ds" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "net" {
+  name          = var.network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.ds.id
+
+  num_cpus = 1
+  memory   = 1024
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.net.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks[0].size
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = var.vm_name
+        domain    = "local"
+      }
+
+      network_interface {
+        ipv4_address = "192.168.1.150"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "192.168.88.1"
     }
   }
-}
-
-provider "oci" {
-  region              = "us-sanjose-1"
-  auth                = "SecurityToken"
-  config_file_profile = "learn-terraform"
-}
-
-resource "oci_core_vcn" "internal" {
-  dns_label      = "internal"
-  cidr_block     = "172.16.0.0/20"
-  compartment_id = "<your_compartment_OCID_here>"
-  display_name   = "My internal VCN"
 }
