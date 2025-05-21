8810458e4e66225e700bde26106b7b2b1426fbb4
MATIN0023 <57063111+MATIN0023@users.noreply.github.com>
2025-05-21 15:35:42 +0330

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+
+variable "datacenter" {}
+variable "datastore" {}
+variable "network" {}
+variable "resource_pool" {}
+variable "template_name" {}
+variable "vm_name" {
+  default = "ubuntu-2404-tf"
+}
