8f4c4fb5380bd7ab56f319ff4dc409ab7d8975b6
MATIN0023 <57063111+MATIN0023@users.noreply.github.com>
2025-05-25 11:45:18 +0330

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     vsphere = {
-      source  = "hashicorp/vsphere"
-      version = ">= 2.2.0"
+      source  = "vmware/vsphere"
+      version = ">= 2.13.0"
     }
   }
 }
