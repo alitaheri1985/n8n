756434c6c2f058525cc25d34fd2a6d1ac975c714
MATIN0023 <57063111+MATIN0023@users.noreply.github.com>
2025-05-25 09:11:22 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,50 @@
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+
+variable "datacenter" {
+  default = "your-datacenter-name"
+}
+
+variable "datastore" {
+  default = "your-datastore-name"
+}
+
+variable "cluster" {
+  default = "your-cluster-name"
+}
+
+variable "template_name" {
+  default = "ubuntu-template"
+}
+
+variable "network_name" {
+  default = "VM Network"
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
 data "vsphere_datacenter" "dc" {
   name = var.datacenter
 }
 
-data "vsphere_datastore" "ds" {
+data "vsphere_datastore" "datastore" {
   name          = var.datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
-  name          = var.resource_pool
+  name          = var.cluster
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_network" "net" {
-  name          = var.network
+data "vsphere_network" "network" {
+  name          = var.network_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
@@ -22,27 +53,28 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_name
+resource "vsphere_virtual_machine" "ubuntu_vm" {
+  count            = 6
+  name             = "ubuntu-vm-${count.index + 1}"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.ds.id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
-  num_cpus = 1
-  memory   = 1024
+  num_cpus = 2
+  memory   = 2048
   guest_id = data.vsphere_virtual_machine.template.guest_id
 
   scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
-    network_id   = data.vsphere_network.net.id
+    network_id   = data.vsphere_network.network.id
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
   }
 
   disk {
     label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks[0].size
+    size             = data.vsphere_virtual_machine.template.disks.0.size
     eagerly_scrub    = false
-    thin_provisioned = true
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
 
   clone {
@@ -50,16 +82,16 @@ resource "vsphere_virtual_machine" "vm" {
 
     customize {
       linux_options {
-        host_name = var.vm_name
+        host_name = "ubuntu-vm-${count.index + 1}"
         domain    = "local"
       }
 
       network_interface {
-        ipv4_address = "192.168.1.150"
+        ipv4_address = "192.168.1.${100 + count.index}"
         ipv4_netmask = 24
       }
 
-      ipv4_gateway = "192.168.88.1"
+      ipv4_gateway = "192.168.1.1"
     }
   }
 }
